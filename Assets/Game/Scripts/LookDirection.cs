84c5948d10951bbbb6000e1e971a2655d962c95f
SINDHUJARA0BALGURI <84570484+SINDHUJARA0BALGURI@users.noreply.github.com>
2021-08-25 15:34:22 +0530

    Looking in Y direction is done

--- Assets/Game/Scripts/LookDirection.cs
+++ Assets/Game/Scripts/LookDirection.cs
@@ -16,5 +16,8 @@ public class LookDirection : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X");
         transform.localEulerAngles = new Vector3(transform.localEulerAngles.x,( transform.localEulerAngles.y + mouseX)*rotateSpeed, transform.localEulerAngles.z);
+    
+    
+    
     }
 }
