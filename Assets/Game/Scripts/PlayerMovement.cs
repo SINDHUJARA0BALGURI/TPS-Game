bb802c7f7533588f1d5dfa008051721914ed4862
SINDHUJARA0BALGURI <84570484+SINDHUJARA0BALGURI@users.noreply.github.com>
2021-08-25 15:28:23 +0530

    made player to look in sideWays

--- Assets/Game/Scripts/PlayerMovement.cs
+++ Assets/Game/Scripts/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     private CharacterController character;
     [SerializeField]
     private float playerSpeed = 5.0f;
+    private float gravity = 9.81f;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +17,18 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Movement();
+    }
+    private void Movement()
+    {
+
         Vector3 direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        character.Move(direction * Time.deltaTime*playerSpeed);
-        
+        Vector3 velocity = direction * playerSpeed;
+        velocity.y -= gravity;
+        velocity = transform.transform.TransformDirection(velocity);//it converts local space to world space direction
+
+
+        character.Move(direction * Time.deltaTime * playerSpeed);
+
     }
 }
