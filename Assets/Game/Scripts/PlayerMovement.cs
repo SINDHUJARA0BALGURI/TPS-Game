8baf63ab5e483b17d9d1935ed27bea4a4ac407a5
SINDHUJARA0BALGURI <84570484+SINDHUJARA0BALGURI@users.noreply.github.com>
2021-08-25 16:43:06 +0530

    ParticleEffects are added

--- Assets/Game/Scripts/PlayerMovement.cs
+++ Assets/Game/Scripts/PlayerMovement.cs
@@ -8,6 +8,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float playerSpeed = 5.0f;
     private float gravity = 9.81f;
+    [SerializeField]
+    private GameObject particleEffectPrefab;
+    [SerializeField]
+    private GameObject hitMarketParticlePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +22,26 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         Movement();
+        //raycast from centre of main camera
+       
+        if (Input.GetMouseButton(0))
+        {
+            particleEffectPrefab.SetActive(true);
+
+            Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+            if (Physics.Raycast(ray,out hit,Mathf.Infinity))
+            {
+                Debug.Log("Raycast got hit"+hit.transform.name);
+
+                Instantiate(hitMarketParticlePrefab,hit.point,Quaternion.LookRotation(hit.normal));
+
+            }
+        }
+        else
+        {
+            particleEffectPrefab.SetActive(false);
+        }
     }
     private void Movement()
     {
